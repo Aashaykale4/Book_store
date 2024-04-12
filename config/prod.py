63e3de9c9ab5b45fd422dadd2661ab972f8c35e8
Aashaykale263 <105640375+Aashaykale4@users.noreply.github.com>
2024-04-12 21:49:18 +0530

    made corrections

--- config/prod.py
+++ config/prod.py
@@ -1,5 +1,8 @@
+import os
 DEBUG=True
 
 SECRET_KEY='Aashay%40123'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:Aashay%40123@localhost/New'
+# SQLALCHEMY_DATABASE_URI='postgresql://postgres:Aashay%40123@localhost/New'
+SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
+# 'postgresql://postgres:Aashay%40123@localhost/New'
